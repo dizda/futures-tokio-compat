e8f8a6e095eac6a2d4f6351022e5e81d4a2ee7ed
Jonathan Dizdarevic <dizzda@gmail.com>
2020-02-17 15:35:10 +0700

    fix compatibility between futures & tokio

--- src/io.rs
+++ src/io.rs
@@ -1,11 +1,11 @@
 use crate::Compat;
-use core::{
-    pin::Pin,
+use futures_core::{
     task::{self, Poll},
 };
+use std::pin::Pin;
 use std::io;
 
-impl<T> tokio_io::AsyncRead for Compat<T>
+impl<T> tokio::prelude::AsyncRead for Compat<T>
 where
     T: futures_io::AsyncRead,
 {
@@ -20,18 +20,18 @@ where
 
 impl<T> futures_io::AsyncRead for Compat<T>
 where
-    T: tokio_io::AsyncRead,
+    T: tokio::prelude::AsyncRead,
 {
     fn poll_read(
         self: Pin<&mut Self>,
         cx: &mut task::Context,
         buf: &mut [u8],
     ) -> Poll<io::Result<usize>> {
-        tokio_io::AsyncRead::poll_read(self.project().inner, cx, buf)
+        tokio::prelude::AsyncRead::poll_read(self.project().inner, cx, buf)
     }
 }
 
-impl<T> tokio_io::AsyncBufRead for Compat<T>
+impl<T> tokio::prelude::AsyncBufRead for Compat<T>
 where
     T: futures_io::AsyncBufRead,
 {
@@ -49,21 +49,21 @@ where
 
 impl<T> futures_io::AsyncBufRead for Compat<T>
 where
-    T: tokio_io::AsyncBufRead,
+    T: tokio::prelude::AsyncBufRead,
 {
     fn poll_fill_buf<'a>(
         self: Pin<&'a mut Self>,
         cx: &mut task::Context,
     ) -> Poll<io::Result<&'a [u8]>> {
-        tokio_io::AsyncBufRead::poll_fill_buf(self.project().inner, cx)
+        tokio::prelude::AsyncBufRead::poll_fill_buf(self.project().inner, cx)
     }
 
     fn consume(self: Pin<&mut Self>, amt: usize) {
-        tokio_io::AsyncBufRead::consume(self.project().inner, amt)
+        tokio::prelude::AsyncBufRead::consume(self.project().inner, amt)
     }
 }
 
-impl<T> tokio_io::AsyncWrite for Compat<T>
+impl<T> tokio::prelude::AsyncWrite for Compat<T>
 where
     T: futures_io::AsyncWrite,
 {
@@ -86,21 +86,21 @@ where
 
 impl<T> futures_io::AsyncWrite for Compat<T>
 where
-    T: tokio_io::AsyncWrite,
+    T: tokio::prelude::AsyncWrite,
 {
     fn poll_write(
         self: Pin<&mut Self>,
         cx: &mut task::Context,
         buf: &[u8],
     ) -> Poll<io::Result<usize>> {
-        tokio_io::AsyncWrite::poll_write(self.project().inner, cx, buf)
+        tokio::prelude::AsyncWrite::poll_write(self.project().inner, cx, buf)
     }
 
     fn poll_flush(self: Pin<&mut Self>, cx: &mut task::Context) -> Poll<io::Result<()>> {
-        tokio_io::AsyncWrite::poll_flush(self.project().inner, cx)
+        tokio::prelude::AsyncWrite::poll_flush(self.project().inner, cx)
     }
 
     fn poll_close(self: Pin<&mut Self>, cx: &mut task::Context) -> Poll<io::Result<()>> {
-        tokio_io::AsyncWrite::poll_shutdown(self.project().inner, cx)
+        tokio::prelude::AsyncWrite::poll_shutdown(self.project().inner, cx)
     }
 }
